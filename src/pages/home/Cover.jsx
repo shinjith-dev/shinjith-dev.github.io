621213201427bde37593e7da82fb2465074f3a13
shinjith-dev <shinjithpallikkara@gmail.com>
2023-08-09 14:30:28 +0530

    gradient text

--- src/pages/home/Cover.jsx
+++ src/pages/home/Cover.jsx
@@ -14,15 +14,15 @@ function Cover() {
         className="absolute flex gap-2 tracking-widest flex-col w-[550px] top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]"
         // style={{ margin: "0 max(30vw,1rem)" }}
       >
-        <div className="text-xl text-light-dark font-medium transition-all">
+        <div className="text-xl text-light-dark font-medium transition-all text-gradient-light">
           HEY! I&apos;M
         </div>
-        <div className="text-6xl text-brand font-bold tracking-widest text-gradient-light">
+        <div className="text-6xl text-brand font-bold tracking-widest">
           SHINJITH P R
         </div>
         <div>
           <div
-            className="text-2xl inline font-medium text-light transition-all uppercase"
+            className="text-2xl inline font-medium text-light transition-all uppercase text-gradient-light gradient-animation"
             ref={textRef}
           />
           <div className="text-2xl inline font-medium text-light transition-all">
